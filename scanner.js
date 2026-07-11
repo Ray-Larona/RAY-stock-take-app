f648926c8ee0bdd0e7fcb7f2f2bb77937e46a6a4
Ray-Larona <olaremato@gmail.com>
2026-07-11 22:30:23 +0800

    Create scanner.js

---
+++ scanner.js
@@ -0,0 +1,68 @@
+let scanner;
+
+function startScanner(){
+
+
+document.getElementById("reader").style.display="block";
+
+
+scanner = new Html5Qrcode("reader");
+
+
+scanner.start(
+
+{
+  facingMode:"environment"
+},
+
+{
+  fps:10,
+  qrbox:250
+},
+
+
+(decodedText)=>{
+
+
+console.log("SCAN:",decodedText);
+
+
+// BEEP
+
+let beep = new Audio(
+"https://actions.google.com/sounds/v1/alarms/beep_short.ogg"
+);
+
+beep.play();
+
+
+
+// SHOW RESULT
+
+document.getElementById("barcode").innerHTML =
+decodedText;
+
+
+
+scanner.stop();
+
+
+}
+
+);
+
+
+}
+
+
+
+function stopScanner(){
+
+
+if(scanner){
+
+scanner.stop();
+
+}
+
+}

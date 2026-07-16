2eb374a17cd88dffa67a9a884204b50342f62e44
Ray-Larona <olaremato@gmail.com>
2026-07-16 10:40:15 +0800

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -19,113 +19,74 @@ function startScanner(){
 
 
 
-  Html5Qrcode.getCameras().then(cameras => {
+  scanner.start(
 
-
-    if(cameras && cameras.length){
-
-
-      let cameraId = cameras[0].id;
-
-
-
-      // SELECT BACK CAMERA
-
-      for(let cam of cameras){
-
-        let label = cam.label.toLowerCase();
-
-        if(
-          label.includes("back") ||
-          label.includes("rear") ||
-          label.includes("environment")
-        ){
-
-          cameraId = cam.id;
-
-          break;
-
-        }
-
+    {
+      facingMode:{
+        ideal:"environment"
       }
+    },
+
+    {
+      fps:10,
+      qrbox:250,
+      aspectRatio:1.777
+    },
+
+
+    (decodedText)=>{
+
+
+      console.log("SCAN:", decodedText);
 
 
 
-      scanner.start(
+      // BEEP
 
-        cameraId,
-
-        {
-
-          fps:10,
-
-          qrbox:250,
-
-          aspectRatio:1.777
-
-        },
-
-
-        (decodedText)=>{
-
-
-          console.log("SCAN:", decodedText);
-
-
-
-          // BEEP
-
-          let beep = new Audio(
-            "https://actions.google.com/sounds/v1/alarms/beep_short.ogg"
-          );
-
-          beep.play();
-
-
-
-          // SHOW BARCODE
-
-          document.getElementById("barcode").innerHTML =
-          decodedText;
-
-
-
-          // ADD ITEM
-
-          addBarcode(decodedText);
-
-
-
-          console.log("ADDED:", decodedText);
-
-
-
-          // AUTO CLOSE AFTER SUCCESS SCAN
-
-          scanner.stop().then(()=>{
-
-
-            document.getElementById("cameraBox").style.display="none";
-
-            document.getElementById("closeCameraBtn").style.display="none";
-
-            document.getElementById("scanBtn").style.display="block";
-
-
-          });
-
-
-
-        }
-
-
+      let beep = new Audio(
+        "https://actions.google.com/sounds/v1/alarms/beep_short.ogg"
       );
+
+      beep.play();
+
+
+
+      // SHOW BARCODE
+
+      document.getElementById("barcode").innerHTML =
+      decodedText;
+
+
+
+      // ADD ITEM
+
+      addBarcode(decodedText);
+
+
+
+      console.log("ADDED:", decodedText);
+
+
+
+      // AUTO CLOSE AFTER SUCCESS SCAN
+
+      scanner.stop().then(()=>{
+
+
+        document.getElementById("cameraBox").style.display="none";
+
+        document.getElementById("closeCameraBtn").style.display="none";
+
+        document.getElementById("scanBtn").style.display="block";
+
+
+      });
 
 
     }
 
 
-  }).catch(error=>{
+  ).catch(error=>{
 
     console.log("CAMERA ERROR:",error);
 

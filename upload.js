55a2ea26f1f63027b75f5cae236cdb8504398a27
Ray-Larona <olaremato@gmail.com>
2026-09-14 16:21:05 +0800

    Update upload.js

--- upload.js
+++ upload.js
@@ -5,78 +5,126 @@ function uploadStockTake(){
     return;
   }
 
+
   if(stockItems.length==0){
     alert("No items to upload");
     return;
   }
 
+
   const btn = document.getElementById("uploadBtn");
+
 
   btn.disabled = true;
   btn.innerHTML = "⏳ UPLOADING...";
 
-  // 🔒 I-lock ang buong screen habang nagpapadala sa Google Sheets
-  toggleLockScreen(true, "Uploading stock take, please wait...");
 
   const token = localStorage.getItem("token");
+
 
   fetch(API_URL,{
     method:"POST",
     body:JSON.stringify({
+
       action:"uploadStockTake",
+
       token:token,
+
       location:currentLocation,
+
       items:stockItems
+
     })
+
   })
+
+
   .then(res=>res.json())
+
+
   .then(result=>{
 
+
     if(result.success){
-      
+
+     
       alert(
         "✅ Upload Successful!\n\nBatch: "
         + result.batchID
       );
 
+
       // CLEAR ONLY ITEMS
+
       stockItems = [];
+
+
       localStorage.removeItem("stockItems");
 
+
       // KEEP LOCATION
+
+
       displayItems();
+
 
       const barcodeText =
       document.getElementById("barcode");
 
+
       if(barcodeText){
+
         barcodeText.innerText="---";
+
       }
+
 
       const total =
       document.getElementById("total");
 
+
       if(total){
+
         total.innerText="0";
+
       }
 
-    }
-    else{
-      alert(result.message);
+
     }
 
+    else{
+
+
+      alert(result.message);
+
+
+    }
+
+
   })
+
+
   .catch(err=>{
+
+
     alert(
       "Upload failed.\n" + err
     );
+
+
   })
+
+
   .finally(()=>{
-    // 🔓 Tanggalin ang screen lock at ibalik ang button kapag tapos na (success man o error)
-    toggleLockScreen(false);
+
 
     btn.disabled = false;
-    btn.innerHTML = "📤 UPLOAD LOCATION";
+
+    btn.innerHTML =
+    "📤 UPLOAD LOCATION";
+
+
   });
+
 
 }
